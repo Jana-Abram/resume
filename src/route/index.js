e6dfc7e65afdae567185cac2ecd67b96841e258a
Jana <128377430+jana423151@users.noreply.github.com>
2023-05-24 19:00:18 +0300

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -3025,7 +3025,7 @@ router.get('/shopreview', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/shopcatalog', function (req, res) {
+router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
